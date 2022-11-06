3cb232321a51052df7fbb1dec21de7147c09e405
M0nspiet <Kaliberovnik04@mail.ru>
2022-11-06 20:27:45 +0300

    idz_1 test

---
+++ idz_1/idz_1.js
@@ -0,0 +1,23 @@
+function d(){
+    d = i * i;
+    return d;
+}
+function c(d){
+    let c = d * d;
+    return c;
+}
+function b(c,d){
+    let b = c * c;
+    return b;
+}
+function a(b,c,d){
+    let a = b * b;
+    return b;
+}
+function compose(a,b,c,d){
+    let compose = a * a;
+    return compose;
+}
+let f = compose(a, b, c, d);
+console.log(f(2))
+f();
